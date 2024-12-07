6f4e813451ad87b4be4b5acf760cbbc4c1c1af96
Victor Vasconcelos <victorolimpiovasconcelos0808@gmail.com>
2024-12-07 12:48:07 +0000

    refactor: send a 409 status with a service message in the event of a controller error

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -6,8 +6,8 @@ export const controllerCreateUser = async (userData: CreateUserType, reply: Fast
   try {
     const newUser = await serviceCreateUser(userData);
     return reply.status(201).send({ newUser });
-  } catch (err) {
-    console.error(`error accessing the create user service: ${err}`);
-    return reply.status(500).send({ message: "error accessing the create user service" });
+  } catch (err: any) {
+    console.error(`error accessing the create user service: ${err.message}`);
+    return reply.status(409).send({ message: err.message });
   }
 }
