24d3f72eb21574aca902fc36a8cf195eda02d477
Victor Vasconcelos <victorolimpiovasconcelos0808@gmail.com>
2024-12-08 17:21:31 +0000

    feat: create the login service for the user (with jwt)

--- server/src/services/authService.ts
+++ server/src/services/authService.ts
@@ -1,4 +1,5 @@
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 import type { UserType } from "../types/userType.js";
 import { Prisma, PrismaClient } from "@prisma/client";
 import { handlePrismaError } from "../utils/prismaErrorHandler.js";
@@ -42,5 +43,36 @@ export const serviceRegisterUser = async (userData: UserType) => {
     }
 
     throw new Error(`The user could not be created: ${err.message}`);
+  }
+}
+
+export const serviceLoginUser = async (userData: UserType) => {
+  try {
+    const findUserByEmail = await prisma.user.findUnique({
+      where: { email: userData.email }
+    });
+
+    if (!findUserByEmail)
+      throw new Error('The user could not be found');
+
+    const passwordIsMatch = await bcrypt.compare(userData.password, findUserByEmail.password);
+
+    if (!passwordIsMatch)
+      throw new Error("This password is not valid");
+
+    const jwtSecret = process.env. JWT_SECRET;
+    
+    if (!jwtSecret)
+      throw new Error("This secret token is not valid");
+
+    const tokenJwt = jwt.sign({ id: findUserByEmail.id }, jwtSecret, { expiresIn: "1h" });
+
+    return { tokenJwt };
+  } catch (err: any) {
+    if (err instanceof Prisma.PrismaClientKnownRequestError) {
+      throw new Error(handlePrismaError(err));
+    }
+
+    throw new Error(`The user cannot log in: ${err.message}`);
   }
 }
