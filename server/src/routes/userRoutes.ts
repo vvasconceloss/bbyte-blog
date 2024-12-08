76bd886606a5fa0ba79713efd7fdb7aaa631726b
Victor Vasconcelos <victorolimpiovasconcelos0808@gmail.com>
2024-12-08 13:06:55 +0000

    feat: create a route to update the user's password

--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -1,6 +1,7 @@
 import type { UserType } from "../types/userType.js";
-import { controllerDeleteUser, controllerUpdateUser } from "../controllers/userController.js";
+import type { PasswordType } from "../types/passwordType.js";
 import type { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
+import { controllerDeleteUser, controllerUpdatePassword, controllerUpdateUser } from "../controllers/userController.js";
 
 export async function userRoutes(fastify: FastifyInstance) {
   fastify.delete('/user/:userId', {
@@ -43,6 +44,27 @@ export async function userRoutes(fastify: FastifyInstance) {
       await controllerUpdateUser(userId, request.body, reply);
     } catch (err: any) {
       console.error(`error accessing the updated user controller: ${err}`);
+      process.exit(1);
+    }
+  });
+
+  fastify.put('/user/password/:userId', {
+    schema: {
+      params: {
+        type: 'object',
+        properties: {
+          userId: { type: 'integer' },
+        },
+        required: ['userId'],
+      },
+    },
+  }, async (request: FastifyRequest<{ Params: { userId: number}, Body: PasswordType }>, reply: FastifyReply) => {
+    try {
+      const { userId } = request.params;
+      
+      await controllerUpdatePassword(userId, request.body, reply); 
+    } catch (err: any) {
+      console.error(`error accessing the updated password controller: ${err}`);
       process.exit(1);
     }
   });
