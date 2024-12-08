0e6d7e083e15a99f262960d3483554033ad690e4
Victor Vasconcelos <victorolimpiovasconcelos0808@gmail.com>
2024-12-08 21:43:03 +0000

    feat: create a service to list the posts of a specific user

--- server/src/services/post/postService.ts
+++ server/src/services/post/postService.ts
@@ -1,5 +1,6 @@
 import { PrismaClient, Prisma } from "@prisma/client";
 import type { PostType } from "../../types/postType.js";
+import { serviceFindUser } from "../user/userService.js";
 import { handlePrismaError } from "../../utils/prismaErrorHandler.js";
 
 const prisma = new PrismaClient();
@@ -44,3 +45,25 @@ export const serviceFindAllPosts = async () => {
     throw new Error(`Could not list all posts: ${err.message}`);
   }
 }
+
+export const serviceFindAllUserPosts = async (authorId: number) => {
+  const findAuthor = await serviceFindUser(authorId);
+
+  if (!findAuthor)
+    throw new Error('The user could not be found');
+
+  try {
+    const allUserPosts = await prisma.post.findMany({
+      where: { authorId: authorId },
+      include: { author: true }
+    });
+
+    return allUserPosts;
+  } catch (err: any) {
+    if (err instanceof Prisma.PrismaClientKnownRequestError) {
+      throw new Error(handlePrismaError(err));
+    }
+
+    throw new Error(`Could not list all posts by this user: ${err.message}`);
+  }
+} 
