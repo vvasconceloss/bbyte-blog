a439c560d13f853970ea4307022f30e0a15701cb
Victor Vasconcelos <victorolimpiovasconcelos0808@gmail.com>
2024-12-08 10:58:09 +0000

    feat: create the controller to access the delete user service

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -1,13 +1,23 @@
 import type { FastifyReply } from "fastify";
-import type { CreateUserType } from "../types/createUserType.js";
-import { serviceCreateUser } from "../services/userService.js";
+import type { UserType } from "../types/userType.js";
+import { serviceCreateUser, serviceDeleteUser } from "../services/userService.js";
 
-export const controllerCreateUser = async (userData: CreateUserType, reply: FastifyReply) => {
+export const controllerCreateUser = async (userData: UserType, reply: FastifyReply) => {
   try {
     const newUser = await serviceCreateUser(userData);
     return reply.status(201).send({ newUser });
   } catch (err: any) {
     console.error(`error accessing the create user service: ${err.message}`);
+    return reply.status(409).send({ message: err.message });
+  }
+}
+
+export const controllerDeleteUser = async (userId: number, reply: FastifyReply) => {
+  try {
+    const deleteUser = await serviceDeleteUser(userId);
+    return reply.status(200).send({ message: "user successfully deleted." });
+  } catch (err: any) {
+    console.error(`error accessing the delete user service: ${err.message}`);
     return reply.status(409).send({ message: err.message });
   }
 }
