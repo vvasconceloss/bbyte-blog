f77793316c83c2cd9c4407fd21053dcde6f4bac9
Victor Vasconcelos <victorolimpiovasconcelos0808@gmail.com>
2024-12-08 12:01:27 +0000

    feat: create the controller to access the service to update a user

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -1,7 +1,7 @@
 import type { FastifyReply } from "fastify";
 import type { UserType } from "../types/userType.js";
 import { userSchema } from "../utils/dataValidation.js";
-import { serviceCreateUser, serviceDeleteUser } from "../services/userService.js";
+import { serviceCreateUser, serviceDeleteUser, serviceUpdateUser } from "../services/userService.js";
 
 export const controllerCreateUser = async (userData: UserType, reply: FastifyReply) => {
   const validationData = userSchema.safeParse(userData);
@@ -26,6 +26,16 @@ export const controllerDeleteUser = async (userId: number, reply: FastifyReply) 
     return reply.status(200).send({ message: "user successfully deleted." });
   } catch (err: any) {
     console.error(`error accessing the delete user service: ${err.message}`);
+    return reply.status(409).send({ message: err.message });
+  }
+}
+
+export const controllerUpdateUser = async (userId: number, userData: UserType, reply: FastifyReply) => {
+  try {
+    const updatedUser = await serviceUpdateUser(userId, userData);
+    return reply.status(201).send({ updatedUser })
+  } catch (err: any) {
+    console.error(`error accessing the update user service: ${err.message}`);
     return reply.status(409).send({ message: err.message });
   }
 }
