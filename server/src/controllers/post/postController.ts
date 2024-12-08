616f75a9450cf49646469b7dee2157b503853ed6
Victor Vasconcelos <victorolimpiovasconcelos0808@gmail.com>
2024-12-08 21:43:39 +0000

    feat: create a controller to consume a user's post service

--- server/src/controllers/post/postController.ts
+++ server/src/controllers/post/postController.ts
@@ -1,6 +1,6 @@
 import type { FastifyReply } from "fastify";
 import type { PostType } from "../../types/postType.js";
-import { serviceCreatePost, serviceFindAllPosts } from "../../services/post/postService.js";
+import { serviceCreatePost, serviceFindAllPosts, serviceFindAllUserPosts } from "../../services/post/postService.js";
 
 export const controllerCreatePost = async (postData: PostType, userId: number, reply: FastifyReply) => {
   try {
@@ -18,6 +18,16 @@ export const controllerFindAllPosts = async (reply: FastifyReply) => {
     return reply.status(200).send({ allPosts });
   } catch (err: any) {
     console.error(`error accessing the find all posts service: ${err.message}`);
+    return reply.status(409).send({ message: err.message });
+  }
+}
+
+export const controllerFindAllUserPosts = async (authorId: number, reply: FastifyReply) => {
+  try {
+    const allUserPosts = await serviceFindAllUserPosts(authorId);
+    return reply.status(200).send({ allUserPosts });
+  } catch (err: any) {
+    console.error(`error accessing all posts by this user: ${err.message}`);
     return reply.status(409).send({ message: err.message });
   }
 }
