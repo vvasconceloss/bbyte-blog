4e40b41937c65018bbf53d1b836805723b3fee1e
Victor Vasconcelos <victorolimpiovasconcelos0808@gmail.com>
2024-12-08 12:02:01 +0000

    feat: create the route to update a user's information

--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -1,4 +1,5 @@
-import { controllerDeleteUser } from "../controllers/userController.js";
+import type { UserType } from "../types/userType.js";
+import { controllerDeleteUser, controllerUpdateUser } from "../controllers/userController.js";
 import type { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
 
 export async function userRoutes(fastify: FastifyInstance) {
@@ -19,6 +20,29 @@ export async function userRoutes(fastify: FastifyInstance) {
       await controllerDeleteUser(userId, reply);
     } catch (err: any) {
       console.error(`error accessing the delete user controller: ${err}`);
+      process.exit(1);
+    }
+  });
+
+  fastify.put('/user/:userId', {
+    schema: {
+      params: {
+        type: 'object',
+        properties: {
+          userId: { type: 'integer' },
+          username: { type: 'string' },
+          email: { type: 'string', format: 'email' },
+        },
+        required: ['userId'],
+      },
+    },
+  }, async (request: FastifyRequest<{ Params: { userId: number }, Body: UserType}>, reply: FastifyReply) => {
+    try {
+      const { userId } = request.params;
+      
+      await controllerUpdateUser(userId, request.body, reply);
+    } catch (err: any) {
+      console.error(`error accessing the updated user controller: ${err}`);
       process.exit(1);
     }
   });
