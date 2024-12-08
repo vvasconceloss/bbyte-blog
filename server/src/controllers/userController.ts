0d0f619cd60e7ed3b4ff0fb9bc44e9c97e3955c2
Victor Vasconcelos <victorolimpiovasconcelos0808@gmail.com>
2024-12-08 11:30:01 +0000

    feat: use the zod validation schema in the controller to create users

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -1,8 +1,16 @@
 import type { FastifyReply } from "fastify";
 import type { UserType } from "../types/userType.js";
+import { userSchema } from "../utils/dataValidation.js";
 import { serviceCreateUser, serviceDeleteUser } from "../services/userService.js";
 
 export const controllerCreateUser = async (userData: UserType, reply: FastifyReply) => {
+  const validationData = userSchema.safeParse(userData);
+
+  if (!validationData.success) {
+    console.error(`Validation error: ${JSON.stringify(validationData.error.errors)}`);
+    return reply.status(400).send({ message: 'Validation error', details: validationData.error.errors });
+  }
+
   try {
     const newUser = await serviceCreateUser(userData);
     return reply.status(201).send({ newUser });
@@ -14,7 +22,7 @@ export const controllerCreateUser = async (userData: UserType, reply: FastifyRep
 
 export const controllerDeleteUser = async (userId: number, reply: FastifyReply) => {
   try {
-    const deleteUser = await serviceDeleteUser(userId);
+    await serviceDeleteUser(userId);
     return reply.status(200).send({ message: "user successfully deleted." });
   } catch (err: any) {
     console.error(`error accessing the delete user service: ${err.message}`);
