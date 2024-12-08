7afdc1a2b95edd8efce3fc3a4c60e1d3e572eeb4
Victor Vasconcelos <victorolimpiovasconcelos0808@gmail.com>
2024-12-08 18:14:04 +0000

    feat: create a controller for the user login service

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -1,7 +1,7 @@
 import type { FastifyReply } from "fastify";
 import type { UserType } from "../types/userType.js";
 import { userSchema } from "../utils/dataValidation.js";
-import { serviceRegisterUser } from "../services/authService.js";
+import { serviceLoginUser, serviceRegisterUser } from "../services/authService.js";
 
 export const controllerRegisterUser = async (userData: UserType, reply: FastifyReply) => {
   const validationData = userSchema.safeParse(userData);
@@ -16,6 +16,16 @@ export const controllerRegisterUser = async (userData: UserType, reply: FastifyR
     return reply.status(201).send({ newUser });
   } catch (err: any) {
     console.error(`error accessing the create user service: ${err.message}`);
+    return reply.status(409).send({ message: err.message });
+  }
+}
+
+export const controllerLoginUser = async (userData: UserType, reply: FastifyReply) => {
+  try {
+    const token = await serviceLoginUser(userData);
+    return reply.status(201).send({ token });
+  } catch (err: any) {
+    console.error(`error accessing the login user service: ${err.message}`);
     return reply.status(409).send({ message: err.message });
   }
 }
