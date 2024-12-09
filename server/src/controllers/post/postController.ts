bd911a1f65de0ab363838ab32832d94521a071a5
Victor Vasconcelos <victorolimpiovasconcelos0808@gmail.com>
2024-12-09 12:05:17 +0000

    feat: create a controller to access the service of updating a post

--- server/src/controllers/post/postController.ts
+++ server/src/controllers/post/postController.ts
@@ -1,6 +1,6 @@
 import type { FastifyReply } from "fastify";
 import type { PostType } from "../../types/postType.js";
-import { serviceCreatePost, serviceFindAllPosts, serviceFindAllUserPosts } from "../../services/post/postService.js";
+import { serviceCreatePost, serviceFindAllPosts, serviceFindAllUserPosts, serviceUpdatePost } from "../../services/post/postService.js";
 
 export const controllerCreatePost = async (postData: PostType, userId: number, reply: FastifyReply) => {
   try {
@@ -28,6 +28,16 @@ export const controllerFindAllUserPosts = async (authorId: number, reply: Fastif
     return reply.status(200).send({ allUserPosts });
   } catch (err: any) {
     console.error(`error accessing all posts by this user: ${err.message}`);
+    return reply.status(409).send({ message: err.message });
+  }
+}
+
+export const controllerUpdatePost = async (postData: PostType, postId: number, authorId: number, reply: FastifyReply) => {
+  try {
+    const updatedPost = await serviceUpdatePost(postData, postId, authorId);
+    return reply.status(200).send({ updatedPost });
+  } catch (err: any) {
+    console.error(`error accessing the update post service: ${err.message}`);
     return reply.status(409).send({ message: err.message });
   }
 }
