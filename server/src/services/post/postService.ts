c0f2eacd1d620b05a436c34823563b375c91cafe
Victor Vasconcelos <victorolimpiovasconcelos0808@gmail.com>
2024-12-09 12:01:58 +0000

    feat: create a service to update a post

--- server/src/services/post/postService.ts
+++ server/src/services/post/postService.ts
@@ -66,4 +66,53 @@ export const serviceFindAllUserPosts = async (authorId: number) => {
 
     throw new Error(`Could not list all posts by this user: ${err.message}`);
   }
-} 
+}
+
+export const serviceFindPostById = async (postId: number) => {
+  const postExist = await prisma.post.findUnique({
+    where: { id: postId }
+  });
+
+  if (!postExist)
+    throw new Error("This post doesn't exist");
+
+  try {
+    const postData = await prisma.post.findUnique({
+      where: { id: postId },
+      include: { author: true }
+    });
+
+    return postData;
+  } catch (err) {
+
+  }
+}
+
+export const serviceUpdatePost = async (postData: PostType, postId: number, authorId: number) => {
+  const findPost = await serviceFindPostById(postId);
+
+  if (!findPost)
+    throw new Error("This post was not found");
+
+  if (findPost.authorId !== authorId)
+    throw new Error("No permission to edit this post");
+
+  try {
+    const updatedPost = await prisma.post.update({
+      where: { id: postId },
+      data: {
+        title: postData.title,
+        content: postData.content
+      },
+      include: { author: true }
+    });
+
+    return updatedPost;
+  } catch (err: any) {
+    if (err instanceof Prisma.PrismaClientKnownRequestError) {
+      throw new Error(handlePrismaError(err));
+    }
+
+    throw new Error(`The post could not be updated: ${err.message}`);
+  }
+}
