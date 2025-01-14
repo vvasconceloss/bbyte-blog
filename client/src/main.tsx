2ad4176553ff4e908f54bea72407eb8135e0cd9f
Victor Vasconcelos <victorolimpiovasconcelos0808@gmail.com>
2025-01-14 19:18:31 +0000

    feat: import the router into main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,4 +1,5 @@
 import './styles/index.css';
+import { Router } from './routes/router';
 import { createRoot } from 'react-dom/client';
 
-createRoot(document.getElementById('root')!).render();
+createRoot(document.getElementById('root')!).render(<Router />);
